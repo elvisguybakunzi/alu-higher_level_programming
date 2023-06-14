7ba2cd43a096a7a1b29d7eca2843c825784a6d1e
elvisguybakunzi <e.bakunzi@alustudent.com>
2023-06-14 15:21:05 -0700

    adding some codes

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 /*  a script that prints a message depending of the number of arguments passed  */
 
-const argsNumber = process.argv.length-2;
+const argsNumber = process.argv.length - 2;
 
 if (argsNumber === 0) {
   console.log('No argument');
-
 } else if (argsNumber === 1) {
   console.log('Argument found');
 } else {
