5a039d573d47b10fba6e56f8a448afd80dd8df50
elvisguybakunzi <e.bakunzi@alustudent.com>
2023-06-14 15:55:56 -0700

    adding some codes

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* Script that prints first argument passed to it */
+
+const arg = process.argv[2];
+
+if (arg === undefined) {
+  console.log("No argument");
+} else {
+  console.log(arg);
+}
