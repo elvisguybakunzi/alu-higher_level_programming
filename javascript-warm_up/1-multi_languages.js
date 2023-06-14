9f2295509182dc9005ef240f4518ee056dce5374
elvisguybakunzi <e.bakunzi@alustudent.com>
2023-06-14 15:10:55 -0700

    adding some codes

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* a script that prints 3 lines  */
 
-const lineA = "C is fun";
-const lineB = "Python is cool";
-const lineC = "JavaScript is amazing";
+const lineA = 'C is fun';
+const lineB = 'Python is cool';
+const lineC = 'JavaScript is amazing';
 
 console.log(lineA);
 console.log(lineB);
--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,10 +4,10 @@
 const argsNumber = process.argv.length - 2;
 
 if (argsNumber === 0) {
-  console.log("No argument");
+  console.log('No argument');
 
 } else if (argsNumber === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
