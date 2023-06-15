7067fc304ff5881a0098d68b8bfd44e9a2f611c2
elvisguybakunzi <e.bakunzi@alustudent.com>
2023-06-14 18:16:39 -0700

    added some codes

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
 
-/* a script that prints x times “C is fun” where x is number of arguments passed to it */
+/* Write a script that prints x times “C is fun”  */
 
-function printC (x) {
-	  if (!Number(x)) {
-		      console.log('Missing number of occurrences');
-		      return;
-		    }
-	  for (let i = 0; i < x; i++) {
+const arg = process.argv[2];
+const numOccurrences = parseInt(arg);
+
+if (!isNaN(numOccurrences) && numOccurrences > 0) {
+	  for (let i = 0; i < numOccurrences; i++) {
 		      console.log('C is fun');
 		    }
-}
-
-if (process.argv.length > 1) {
-	  printC(Number(process.argv[2]));
 } else {
 	  console.log('Missing number of occurrences');
 }
