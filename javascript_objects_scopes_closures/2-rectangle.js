8d151f64bb4fe1faba41190a18eaef4720fa0899
elvisguybakunzi <e.bakunzi@alustudent.com>
2023-06-16 06:56:06 -0700

    debugging codes in this file

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 
 /*  Write a class Rectangle that defines a rectangle:  */
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
+ class Rectangle {
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
     }
-
-    this.width = w;
-    this.height = h;
   }
-}
 
 module.exports = Rectangle;
